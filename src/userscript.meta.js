fca15209074eb3c7fa595ead2bcc6dee4fb28afd
chinnsenn <iamchinnsenn@gmail.com>
2026-07-25 10:28:31 +0900

    Release userscript 1.0.2

--- src/userscript.meta.js
+++ src/userscript.meta.js
@@ -9,7 +9,7 @@
 // @supportURL          https://greasyfork.org/zh-CN/scripts/4870-maximize-video/feedback
 // @match               *://*/*
 // @exclude             *www.w3school.com.cn*
-// @version             1.0.1
+// @version             1.0.2
 // @run-at              document-start
 // ==/UserScript==
 
